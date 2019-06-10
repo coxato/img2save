06cbd0dcf5e2567651117b1b2964773d1415ab88
Carlos Martinez <edu6jas@gmail.com>
2019-06-10 03:47:12 -0400

    git change

--- fileSaveTest.js
+++
@@ -1,2 +0,0 @@
-import saveAs  from 'file-saver';
-saveAs('http://as01.epimg.net/epik/imagenes/2018/04/28/portada/1524913221_572475_1524913364_noticia_normal.jpg', 'foto.jpg'); 
--- src/components/downloadingModal.jsx
+++ src/components/downloadingModal.jsx
@@ -1,5 +1,4 @@
 import React, { Component} from 'react';
-//import { createPortal } from "react-dom";
 // components
 import Loader from './loader';
 // style
